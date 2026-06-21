5d89e72554760fccc421ebfed5bc07703da5e232
mustafaf805l-ship-it <mustafaf805l@gmail.com>
2026-06-21 04:12:32 +0300

    Add Telegram bot functionality to send signals

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import requests
 
+TOKEN = "ضع_التوكن_هنا"
+CHAT_ID = "ضع_الشات_آيدي_هنا"
+
+message = """
+🟢 GOLD SIGNAL
+
+BUY XAUUSD
+
+SL: 5$
+TP: 10$
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("Signal sent")
